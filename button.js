03c4a6144494ea5b4a2c7bc4c94cfc45c603a779
tyooma <flying.alien.godzilla@gmail.com>
2020-06-21 18:01:11 +0300

    Form with classes fixed

---
+++ button.js
@@ -0,0 +1,8 @@
+class Button {
+  constructor() {
+    this.button = document.createElement('button');
+    this.button.type = 'submit'
+    this.button.textContent = 'OK';
+    this.button.className = 'button';
+  }
+}
